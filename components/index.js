47935ba938ea2f7e7c186bafa1e3619abbccb5c7
Chandrashekhar Marella <chandrashekhar.marella@outlook.com>
2022-10-16 20:14:18 +0530

    Completed UI parts - Header, Footer, Banner, Top Sellers, Hot Bids, Create NFT Page

---
+++ components/index.js
@@ -0,0 +1,17 @@
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Button from './Button';
+import Banner from './Banner';
+import CreatorCard from './CreatorCard';
+import NFTCard from './NFTCard';
+import Input from './Input';
+
+export {
+  Navbar,
+  Footer,
+  Button,
+  CreatorCard,
+  Banner,
+  NFTCard,
+  Input,
+};
