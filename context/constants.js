97aa3907e7eb09b8a8a7185347ecb41cb849416a
Chandrashekhar Marella <chandrashekhar.marella@outlook.com>
2022-10-24 13:00:37 +0530

    Hardhat Setup & Smart Contract creation and Deployment completed

---
+++ context/constants.js
@@ -0,0 +1,6 @@
+// In this example, NFTMarketplace got deployed to Contract address: 0x5FbDB2315678afecb367f032d93F642f64180aa3
+
+import marketAbi from './NFTMarketplace.json';
+
+export const MarketAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+export const MarketAddressABI = marketAbi.abi;
